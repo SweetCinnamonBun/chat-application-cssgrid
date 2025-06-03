22f4f7be1b1f99c07b8b83be9edc6ad246424204
Roman <roman.klemiato18@gmail.com>
2025-06-03 19:31:53 +0300

    text-wrapping stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css"
 import { IoTrashOutline } from "react-icons/io5";
 import { GrAttachment } from "react-icons/gr";
+import PfpOne from "./assets/images/profile-pic-1.jpg";
 
 function App() {
   
@@ -13,7 +14,18 @@ function App() {
             <input id="search-box" type="text" placeholder="Search" className="text-black outline-0 bg-white font-bold rounded-xs h-8 border-0 pl-12 pr-5 text-sm"/>
           </div>
           <div id="conversation-list" className="bg-blue-primary">
-            This is the conversation list
+            <div id="conversation" className="grid grid-cols-[40px_1fr_max-content] gap-2.5 text-white text-sm border-b border-blue-darker  pt-5 pl-5 pb-5 pr-4 hover:bg-blue-darker cursor-pointer">
+              <img src={PfpOne} alt="profile-1" className="h-10 rounded-full row-span-2 w-10"/>
+              <div id="title-text" className="font-bold text-[#eee] pl-1 text-md whitespace-nowrap overflow-x-hidden overflow-ellipsis">
+                Hannah River asdfasfsdafsadfsdf
+              </div>
+              <div id="created-date" className="text-[#ddd] whitespace-nowrap text-xs">
+                Apr 16
+              </div>
+              <div id="conversation-message" className="col-span-2 pl-1 text-[13px] whitespace-nowrap overflow-x-hidden overflow-ellipsis">
+                  This is a message jdasflöfjk sdafdasfsdaf
+              </div>
+            </div>
           </div>
           <div id="new-message-container" className="bg-blue-primary border border-t rounded-bl-xl px-4 grid grid-cols-[40px] grid-rows-[40px] content-center">
             <a href="#" className="bg-[#eee] rounded-full text-blue-primary no-underline text-4xl grid place-items-center">+</a>
